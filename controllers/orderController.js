212ce99c9a884fbcc6c13958c9def09cff0adcb5
Jean Linares <jean.lnrs@outlook.com>
2020-07-07 01:35:44 -0500

    paypal ok

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,6 +2,13 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const Order=  mongoose.model('Order');
+const paypal = require('paypal-rest-sdk');
+
+paypal.configure({
+    'mode': 'sandbox', //sandbox or live
+    'client_id': 'AaSDE6qINg9wWMlZNBn1-2hl2ykTXot7weQxtMCUeBqNP_jYjp2kYwt64AeGiHrbh1U_sG57m2AzT87B',
+    'client_secret': 'EBe_1tSZYvwKwZBSgwkdNDsedg6BjIE5trrtqtZHqlJPsCoWac05feps9NeArEaP35j7vgr-l_VUS-0i'
+  });
 
 var router= express.Router();
 mongoose.set('useFindAndModify',false);
@@ -44,10 +51,34 @@ router.get('/order/delete/:id',(req,res)=>{
         }
     });
 });
-
+router.get('/success', (req, res) => {
+    const payerId = req.query.PayerID;
+    const paymentId = req.query.paymentId;
+  
+    const execute_payment_json = {
+      "payer_id": payerId,
+      "transactions": [{
+        "amount": {
+          "currency": "USD",
+          "total": "25.00"
+        }
+      }]
+    };
+  
+    paypal.payment.execute(paymentId, execute_payment_json, function (error, payment) {
+      if (error) {
+        console.log(error.response);
+        throw error;
+      } else {
+        console.log(JSON.stringify(payment));
+        res.render('success');
+      }
+    });
+  });
 // POST
 router.post('/cart',(req,res)=>{
-    insertOrder(req,res);
+    //insertOrder(req,res);
+    disque(req,res);
 });
 router.post('/order',(req,res)=>{
     updateOrder(req,res);
@@ -79,6 +110,47 @@ function insertOrder(req,res) {
             console.log('Error insertOrder: '+err);
         }
     });
+}
+
+function disque(req,res) {
+    const create_payment_json = {
+        "intent": "sale",
+        "payer": {
+          "payment_method": "paypal"
+        },
+        "redirect_urls": {
+          "return_url": "http://localhost:4000/success",
+          "cancel_url": "http://localhost:4000/"
+        },
+        "transactions": [{
+          "item_list": {
+            "items": [{
+              "name": "Corinthians",
+              "sku": "001",
+              "price": "2",
+              "currency": "USD",
+              "quantity": 1
+            }]
+          },
+          "amount": {
+            "currency": "USD",
+            "total": "2"
+          },
+          "description": "The best football team EVER!"
+        }]
+      };
+      
+      paypal.payment.create(create_payment_json, function (error, payment) {
+        if (error) {
+          throw error;
+        } else {
+          for (let i = 0; i < payment.links.length; i++) {
+            if (payment.links[i].rel === 'approval_url') {
+              res.redirect(payment.links[i].href);
+            }
+          }
+        }
+      });
 }
 
 module.exports=router;
