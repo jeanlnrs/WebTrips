b224e95069fad0739dd05d28c3c63b9728fd851f
Jean Linares <jean.lnrs@outlook.com>
2020-07-10 19:15:18 -0500

    vistas

--- controllers/dataController.js
+++ controllers/dataController.js
@@ -3,17 +3,22 @@ const express= require('express');
 const Data = require('../models/data.model');
 var router= express.Router();
 
-router.get('/adm', async (req,res)=>{
-    const data = await Data.find();
-    res.render('adm', {
-        data
+router.get('/adm',(req,res)=>{
+    Data.find((err,docs)=>{
+        if (!err) {
+            res.render("adm",{
+                data:docs
+            });
+        } else {
+             console.log('Error in Data: '+ err);    
+        }
     });
 });
 
 router.post('/adm/add', async (req,res)=>{
     const data = new Data(req.body);
     await data.save();
-    res.send('bien');
+    res.redirect('/adm');
 });
 
 module.exports=router;
